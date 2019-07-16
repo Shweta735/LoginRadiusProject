6f4e5d49bcff810d74a292a4e43f3484d8edc553
shweta735 <agarwalshweta735@gmail.com>
2019-07-16 23:53:21 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var weatherInfo = require('./js/weatherInfo');
+
+app.set('view engine','ejs');
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.get('/',(req,res)=>{
+	res.render('index')
+})
+app.post('/weather',weatherInfo)
+
+app.listen(3000)
---
+++ js/weatherInfo.js
@@ -0,0 +1,30 @@
+let request = require('request');
+let moment = require('moment');
+
+var weatherInfo = function(req,res){
+	var city = req.body.city;
+	let apiKey = 'ce5bb3c4e2a021672796f3cd338a8348';
+	let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+	request(url,(err,response,body)=>{
+		if(err)
+			res.render('error')
+		else{
+			var body = JSON.parse(body);
+			if(body.cod === 200){
+				var currentTemp = body.main.temp - 273.15; // converting to celsius
+				var weatherType = body.weather[0].description;
+				var maxTemp = body.main.temp_max - 273.15;// converting to celsius
+				var minTemp = body.main.temp_min - 273.15;// converting to celsius
+				var pressure = body.main.pressure;
+				var humidity = body.main.humidity;
+				var sunriseTime = moment.unix(body.sys.sunrise).format('hh:mm:ss a');
+				var sunsetTime =  moment.unix(body.sys.sunset).format('hh:mm:ss a');
+				var weatherObject = {currentTemp: currentTemp,weatherType : weatherType,maxTemp:maxTemp,minTemp: minTemp, pressure : pressure, humidity:humidity,city: city,sunriseTime : sunriseTime,sunsetTime : sunsetTime}
+				res.render('weatherInfo',{weatherObject : weatherObject})
+			}else{
+				res.render('error')
+			}
+		}
+	})
+}
+module.exports = weatherInfo
